89ad4e17e75b4691fec3861776c3c2f03bbea012
Lich <lonedarkwolf.09@gmail.com>
2026-02-28 18:21:10 +0300

    откатываюсь

--- src/scripts/resources/slab_data.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name SlabData
-## Хранит данные о полублоках. Сохраняется при сборке, загружается в игре.
-
-# { "stone_slab": { "bottom_id": 4, "top_id": 5, "full_id": 3 } }
-@export var slab_registry: Dictionary = {}
-
-# { 4: { "name": "stone_slab", "variant": "bottom" }, 5: { ... } }
-@export var slab_id_map: Dictionary = {}

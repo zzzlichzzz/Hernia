23ce28e11a5a79006eb7619af4a7de6fbfed5f00
Lich <lonedarkwolf.09@gmail.com>
2026-03-01 17:23:32 +0300

    работа над фрейм блоком

--- src/scripts/entities/player/player.gd
+++ src/scripts/entities/player/player.gd
@@ -19,6 +19,10 @@ extends CharacterBody3D
 func _ready() -> void:
 	# Захватываем мышь для управления камерой
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	# Настраиваем коллизию: слой 1 (террейн) + слой 2 (frame-блоки)
+	collision_layer = 1  # Сам игрок на слое 1
+	collision_mask = 3  # Проверяем коллизии со слоями 1 и 2
 
 func _input(event: InputEvent) -> void:
 	# Вращение камеры мышью

e64656760d20fb15eee8f370aa93e9103d5e669b
Lich <lonedarkwolf.09@gmail.com>
2026-03-07 14:06:56 +0300

    игнорирование файла автосборщика

--- src/scripts/network/generated/generated_packets.gd
+++ src/scripts/network/generated/generated_packets.gd
@@ -1,7 +1,7 @@
 # ═══════════════════════════════════════════════════
 # AUTO-GENERATED — DO NOT EDIT
 # Source: res://src/scripts/network/actions/
-# Date:   2026-03-07T01:06:45
+# Date:   2026-03-07T14:03:57
 # ═══════════════════════════════════════════════════
 class_name GeneratedPackets
 
